81652ad9cffe2a916e311cf54f13413474566eb4
jun <jun020216@naver.com>
2022-07-11 13:53:59 +0900

    initial commit

---
+++ backend/src/main/java/codeit/cakeN/CakeNApplication.java
@@ -0,0 +1,13 @@
+package codeit.cakeN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeNApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeNApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/codeit/cakeN/CakeNApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.cakeN;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeNApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
