31c02bd278afbcba92692c098416307656848227
{wonhee} <wanny416@sookmyung.ac.kr>
2022-08-04 23:06:09 +0900

    [FEAT]레터링 추천 페이지에 찜한 레터링 목록으로 가는 버튼

--- frontend/cake/src/components/NaviHeader.js
+++ frontend/cake/src/components/NaviHeader.js
@@ -1,19 +1,33 @@
 const NaviHeader = () => {
+  return (
+    <header className="NaviHeader" id="navbar">
+      <div className="left">
+        <a href={"/"} id="logo">
+          cakeN
+        </a>
+        <a href="/intro" className="menu">
+          사이트 소개
+        </a>
+        <a href="/design" className="menu">
+          케이크 제작
+        </a>
+        <a href="/contest" className="menu">
+          케이크 자랑
+        </a>
+        <a href="/letter" className="menu">
+          레터링 선택
+        </a>
+      </div>
+      <div className="right">
+        <a href={"/login"} className="menu">
+          로그인
+        </a>
+        <a href={"/signUp"} className="menu">
+          회원가입
+        </a>
+      </div>
+    </header>
+  );
+};
 
-    return (
-      <header className="NaviHeader">
-          <div className="left">
-              <a href={"/"} id="logo">cakeN</a>
-              <a href="/intro" className="menu">사이트 소개</a>
-              <a href="/design" className="menu">케이크 제작</a>
-              <a href="contest" className="menu">케이크 자랑</a>
-          </div>
-          <div className="right">
-              <a href={"/login"} className="menu">로그인</a>
-              <a href={"/signUp"} className="menu">회원가입</a>
-          </div>
-      </header>
-    );
-  };
-  
-  export default NaviHeader;
+export default NaviHeader;
