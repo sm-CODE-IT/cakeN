7a56ad28ecd3ed21cf66787df5a3babe354e77dd
jun <jun020216@naver.com>
2022-08-11 21:10:48 +0900

    [FEAT] Form에서 업로드한 파일 저장

---
+++ backend/src/main/java/codeit/cakeN/web/test/ServletUploadControllerV2.java
@@ -0,0 +1,78 @@
+package codeit.cakeN.web.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+
+@Slf4j
+@Controller
+@RequestMapping("/servlet/v2")
+public class ServletUploadControllerV2 {
+
+    @Value("${codeit.cakeN.upload.path}")
+    private String filePath;
+
+    @GetMapping("/upload")
+    public String newFile() {
+        return "upload-form";
+    }
+
+    @PostMapping("/upload")
+    public String saveFileV1(HttpServletRequest request) throws ServletException, IOException {
+        log.info("request={}", request);
+
+        String itemName = request.getParameter("itemName");
+        log.info("itemName={}", itemName);
+
+        Collection<Part> parts = null;
+        try {
+            parts = request.getParts();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
+        }
+        log.info("parts={}", parts);
+
+        for (Part part : parts) {
+            log.info("==== PART ====");
+            log.info("name={}", part.getName());
+            Collection<String> headerNames = part.getHeaderNames();
+            for (String headerName : headerNames) {
+                log.info("header {}: {}", headerName, part.getHeader(headerName));
+            }
+
+            // 편의 메서드
+            // content-disposition; filename
+            log.info("submittedFileName={}", part.getSubmittedFileName());
+            log.info("size={}", part.getSize());    // part body size
+
+            // 데이터 읽기
+            InputStream inputStream = part.getInputStream();
+            String body = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+            log.info("body={}", body);
+
+            // 파일 저장하기
+            if (StringUtils.hasText(part.getSubmittedFileName())) {
+                String fullPath = filePath + part.getSubmittedFileName();
+                log.info("파일 저장 fullPath={}", fullPath);
+                part.write(fullPath);
+            }
+        }
+
+        return "upload-form";
+    }
+}
