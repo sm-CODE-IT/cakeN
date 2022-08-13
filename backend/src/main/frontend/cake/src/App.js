ba0bd818d4f649c757b75de8294a11336bec7575
jun <jun020216@naver.com>
2022-08-13 15:00:36 +0900

    Back -> Front Data 넘겨주기 위한 Settings

--- backend/src/main/frontend/cake/src/App.js
+++ backend/src/main/frontend/cake/src/App.js
@@ -11,6 +11,8 @@ import SignUp from './pages/SignUp';
 import Lettering from './pages/Lettering';
 import MyPage from './pages/MyPage';
 import MyCake from './pages/MyCake';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 
 function App() {
@@ -30,6 +32,27 @@ function App() {
     }
   };
 
+  
+  /**
+   * axios 방식을 이용한 Back 데이터 가져오기
+   */
+  // 요청 받은 정보를 담아줄 변수 선언
+  const [testStr, setTestStr] = useState('')
+
+  // 변수 초기화
+  function callback(str) {
+    setTestStr(str);
+  }
+  
+  // 첫 번째 렌더링을 마친 후 실행
+  useEffect(() => {
+    axios({
+      url: '/api/hello',
+      method: 'GET'
+    }).then(response => callback(response.data))
+        .catch(error => console.log(error))
+  }, []);
+
   return (
     <div className="App">
       <NaviHeader />
@@ -46,6 +69,8 @@ function App() {
           <Route path='/myCake' element={<MyCake />} />
         </Routes>
       </BrowserRouter>
+
+      {testStr}
     </div>
   );
 }
