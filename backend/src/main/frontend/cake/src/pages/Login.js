514e02fe4ace5e3a904cd4727faf60313c0423c3
{wonhee} <wanny416@sookmyung.ac.kr>
2022-08-23 03:02:08 +0900

    [FEAT] 소셜로그인까지 완성

--- backend/src/main/frontend/cake/src/pages/Login.js
+++ backend/src/main/frontend/cake/src/pages/Login.js
@@ -7,7 +7,13 @@ const Login = () => {
   return (
     <div className="Login">
       <ContentHeader title={title} content={content} />
+      <br />
+      <div className="cakeWrapper">
+        <br />
+        <img className="cake" src="https://ifh.cc/g/fOMQxW.png" />
+      </div>
       <div className="form">
+        <br />
         <form>
           <div className="loginField">
             <div>
@@ -22,19 +28,36 @@ const Login = () => {
             </button>
           </div>
         </form>
+        <br />
+        <br />
+        <br />
+        <img className="dish" src="https://ifh.cc/g/gdt06p.jpg" />
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="socialWrapper">
+        <h4 className="socialTitle">간편 로그인</h4>
 
-      <div>
-        <div>
-          <button>네이버로 로그인</button>
+        <div className="divNaver">
+          <button className="socialButton naverButton">
+            <img className="naverImg" src="https://ifh.cc/g/jfgfkw.png" />
+          </button>
         </div>
-        <div>
-          <button>구글로 로그인</button>
+        <div className="divGoogle">
+          <button className="socialButton googleButton">
+            <img className="googleImg" src="https://ifh.cc/g/HaRDS7.jpg" />
+          </button>
         </div>
-        <div>
-          <button>카카오로 로그인</button>
+        <div className="divKakao">
+          <button className="socialButton kakaoButton">
+            <img className="kakaoImg" src="https://ifh.cc/g/vG8GLK.png" />
+          </button>
         </div>
       </div>
+      <br />
+      <br />
     </div>
   );
 };
