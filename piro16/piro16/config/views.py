5a93559e764066c2a465f42917ed0ed57388d166
Jun <80024278+jun02160@users.noreply.github.com>
2022-10-03 14:13:17 +0900

    Delete piro16 directory

--- piro16/piro16/config/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("<h1>Index Pirogramming16 Venv Session")
--- piro16/piro16/hello/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import *   #.은 같은 디렉토리에 있는 파일임을 명시
-
-urlpatterns = [
-    path('', hello, name="hello"),  #hello라는 명령이 들어오면 hello 앱 url 파일로 찾아가겠다!
-]
