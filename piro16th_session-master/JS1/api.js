d13514c39b436cd2d2ecaf414634c6483f23ece7
Jun <80024278+jun02160@users.noreply.github.com>
2022-10-03 14:13:29 +0900

    Delete piro16th_session-master directory

--- piro16th_session-master/JS1/api.js
+++
@@ -1,9 +0,0 @@
-axios.get('https://jsonplaceholder.typicode.com/users', { params: { name: 'Leanne Graham' } })
-.then(function (response) {
-  // handle success
-  console.log(response, 'success');
-})
-.catch(function (error) {
-  // handle error
-  console.log(error, 'error');
-})
